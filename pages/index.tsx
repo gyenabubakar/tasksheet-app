979e5c34e8698b24db478f55cccc0df06d6ffbdf
Gyen Abubakar <indehyde@gmail.com>
2022-02-04 19:49:14 +0000

    setup tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>TaskSheet - Simple, straight-forward task management solution</title>
-                <meta name="description" content="A web application for creating and managing tasks."/>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>
+          TaskSheet - Simple, straight-forward task management solution
+        </title>
+        <meta
+          name="description"
+          content="A web application for creating and managing tasks."
+        />
+      </Head>
 
-            <main className=''>
-            </main>
-        </>
-    )
-}
+      <main className=""></main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
