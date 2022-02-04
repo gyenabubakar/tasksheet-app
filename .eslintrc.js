53ee2d57ad09129a5699a58bae88d81f7565cb95
Gyen Abubakar <indehyde@gmail.com>
2022-02-04 19:06:58 +0000

    setup codestyle with eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+    "prettier",
+    "airbnb",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "import/prefer-default-export": 0,
+    "react/require-default-props": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/function-component-definition": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-unused-vars": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "react/jsx-props-no-spreading": 0,
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  globals: {
+    JSX: true,
+  },
+};
