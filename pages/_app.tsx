097a32cbda5f038b90169c05e8706ec8e157f149
Gyen Abubakar <indehyde@gmail.com>
2022-02-05 16:48:40 +0000

    added progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 import '../styles/globals.css';
 import { PageWithLayout } from '~/assets/ts/types';
@@ -16,6 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/svg" href="/images/logo.svg" />
       </Head>
+
+      <NextNProgress
+        color="#5C68FF"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={6}
+        showOnShallow
+      />
 
       <Layout>
         <Component {...pageProps} />
