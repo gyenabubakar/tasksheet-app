a371005a8b6c0c458c820e7f0bb1477256308c18
Gyen Abubakar <indehyde@gmail.com>
2022-02-05 00:52:29 +0000

    tailwind: added custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     './context/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      main: '#5C68FF',
+      darkgray: '#7A7A7A',
+      lightgray: '#E8E8E8',
+      fakeblack: '#121212',
+      secondarybg: '#F5F5F5',
+    },
   },
   plugins: [],
 };
