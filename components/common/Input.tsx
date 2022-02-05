ee7e125609c8b083f605a8546990352b73936707
Gyen Abubakar <indehyde@gmail.com>
2022-02-05 00:53:59 +0000

    implemented prop typing for Input

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-type InputProps = Partial<React.HTMLProps<HTMLInputElement>>;
+type InputProps = Partial<
+  React.HTMLProps<HTMLInputElement> & {
+    label: React.ReactNode;
+    wrapperClass: string;
+  }
+>;
 
-const Input: React.FC<InputProps> = ({ className, ...props }) => (
-  <div className={`input-group ${className}`} {...props}>
-    <input type="text" />
+const Input: React.FC<InputProps> = ({
+  type,
+  className = '',
+  label = null,
+  wrapperClass = '',
+  id,
+  ...props
+}) => (
+  <div className={`input-wrapper w-[400px] ${wrapperClass}`}>
+    {label && <label htmlFor={id}>{label}</label>}
+    <br />
+    <input
+      type={type || 'text'}
+      id={id}
+      className={` ${className}`}
+      {...props}
+    />
   </div>
 );
 
