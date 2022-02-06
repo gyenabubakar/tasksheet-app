0ce114b9c215fa1d42b9876be08907ea6a31a3cc
Gyen Abubakar <indehyde@gmail.com>
2022-02-06 11:35:17 +0000

    wrote a custom Button components

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -15,6 +15,7 @@ import Container from '~/components/common/Container';
 import useFormValidation, {
   FormValidationErrors,
 } from '~/hooks/useFormValidation';
+import Button from '~/components/common/Button';
 
 interface FormErrors extends FormValidationErrors {
   name: string | null;
@@ -217,38 +218,12 @@ const SignupPage: PageWithLayout = () => {
               </div>
 
               <div className="text-center mt-10">
-                <button
-                  type="submit"
-                  disabled={!formIsValid}
-                  className={`bg-main text-white font-medium px-20 py-4 mx-auto rounded-small flex items-center disabled:opacity-50 disabled:cursor-not-allowed ${
-                    submitting || !formIsValid ? '' : 'hover:bg-darkmain'
-                  }`}
+                <Button
+                  loading={submitting}
+                  disabled={submitting || !formIsValid}
                 >
-                  {submitting && (
-                    <svg
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      />
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      />
-                    </svg>
-                  )}
-
                   {submitting ? 'Hang on...' : 'Sign up'}
-                </button>
+                </Button>
               </div>
             </div>
           </form>
