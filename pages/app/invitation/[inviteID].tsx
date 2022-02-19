981af22e03f52c9e0b016aa267807c51719e161b
Gyen Abubakar <indehyde@gmail.com>
2022-02-19 10:13:09 +0000

    expired invite fallback

--- pages/app/invitation/[inviteID].tsx
+++ pages/app/invitation/[inviteID].tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { PageWithLayout } from '~/assets/ts/types';
 import Navigation from '~/components/common/Navigation';
 import subscriberIllustration from '~/assets/illustrations/subscriber.svg';
+import voidIllustration from '~/assets/illustrations/void.svg';
 import Button from '~/components/common/Button';
 import swal from '~/assets/ts/sweetalert';
 import notify from '~/assets/ts/notify';
@@ -15,6 +16,8 @@ const WorkspaceInvitationActionPage: PageWithLayout = () => {
 
   const router = useRouter();
   const { inviteID } = router.query;
+
+  const expiredOrDeclined = true;
 
   function onAcceptInvite() {
     if (!accepting) {
@@ -55,52 +58,65 @@ const WorkspaceInvitationActionPage: PageWithLayout = () => {
         <title>Invitation to join {inviteID} · TaskSheet</title>
       </Head>
 
-      <main>
-        <Navigation backUrl="/app/" />
+      <Navigation />
 
+      <main className="mt-16">
         <div className="w-56 h-56 mx-auto relative">
-          <Image src={subscriberIllustration} layout="fill" />
+          <Image
+            src={!expiredOrDeclined ? subscriberIllustration : voidIllustration}
+            layout="fill"
+          />
         </div>
 
-        <h1 className="text-3xl font-bold text-center w-full md:w-8/12 lg:max-w-[60%] mx-auto">
-          You&apos;ve been invited to join{' '}
-          <span className="text-main">Workspace Name</span>.
+        <h1 className="text-3xl font-bold text-center w-full md:w-8/12 lg:max-w-[60%] mx-auto mt-12">
+          {!expiredOrDeclined ? (
+            <span>
+              You & apos;ve been invited to join{' '}
+              <span className="text-main">Workspace Name</span>.
+            </span>
+          ) : (
+            <span>This invitation has probably been declined or expired.</span>
+          )}
         </h1>
 
-        <div className="inviter mt-4 flex items-center justify-center">
-          <div className="relative w-8 h-8 rounded-full ring-2 ring-white overflow-hidden">
-            <Image
-              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"
-              layout="fill"
-            />
-          </div>
+        {!expiredOrDeclined && (
+          <div className="inviter mt-4 flex items-center justify-center">
+            <div className="relative w-8 h-8 rounded-full ring-2 ring-white overflow-hidden">
+              <Image
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde"
+                layout="fill"
+              />
+            </div>
 
-          <span className="font-medium text-darkgray ml-3">
-            {' '}
-            De Graft Arthur
-          </span>
-        </div>
-
-        <div className="actions mt-16 flex flex-col md:flex-row items-center justify-center">
-          <div className="accept-wrapper">
-            <Button
-              loading={accepting}
-              disabled={accepting}
-              onClick={() => onAcceptInvite()}
-            >
-              {accepting ? 'On it...' : 'Accept'}
-            </Button>
+            <span className="font-medium text-darkgray ml-3">
+              {' '}
+              De Graft Arthur
+            </span>
           </div>
+        )}
 
-          <div className="decline-wrapper">
-            <button
-              className="mt-3 md:mt-0 md:ml-3 text-red-500 px-20 py-4 rounded-small border-2 border-red-500 hover:bg-red-500 hover:text-white font-medium"
-              onClick={() => onDeclineInvite()}
-            >
-              Decline
-            </button>
+        {!expiredOrDeclined && (
+          <div className="actions mt-16 flex flex-col md:flex-row items-center justify-center">
+            <div className="accept-wrapper">
+              <Button
+                loading={accepting}
+                disabled={accepting}
+                onClick={() => onAcceptInvite()}
+              >
+                {accepting ? 'On it...' : 'Accept'}
+              </Button>
+            </div>
+
+            <div className="decline-wrapper">
+              <button
+                className="mt-3 md:mt-0 md:ml-3 text-red-500 px-20 py-4 rounded-small border-2 border-red-500 hover:bg-red-500 hover:text-white font-medium"
+                onClick={() => onDeclineInvite()}
+              >
+                Decline
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </main>
     </>
   );
