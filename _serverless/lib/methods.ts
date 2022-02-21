ba0ebff9ee80854adb31cfababf8f7ecbcb5508d
Gyen Abubakar <indehyde@gmail.com>
2022-02-21 21:52:24 +0000

    removed experimental _serverless/ directory

--- _serverless/lib/methods.ts
+++
@@ -1,9 +0,0 @@
-import HTTPMethod from 'http-method-enum';
-
-const supportedHttpMethods: { [p: string]: HTTPMethod } = {
-  POST: HTTPMethod.POST,
-  GET: HTTPMethod.GET,
-  PUT: HTTPMethod.PATCH,
-};
-
-export default supportedHttpMethods;
