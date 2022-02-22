b535006c4acf8af01da0dd4fdbd400a0c6f5d31c
Gyen Abubakar <indehyde@gmail.com>
2022-02-22 01:28:52 +0000

    refactor code

--- pages/app/_middleware.ts
+++ pages/app/_middleware.ts
@@ -1,13 +1,11 @@
 import { NextMiddleware, NextResponse } from 'next/server';
 
 export const middleware: NextMiddleware = (request) => {
-  console.log(request.page.name);
   if (
     request.page.name &&
     /^\/app\//.test(request.page.name) &&
     !request.cookies.accessToken
   ) {
-    console.log('hi');
     return NextResponse.redirect(
       `/login?redirect=${encodeURIComponent(request.nextUrl.pathname)}`,
     );
