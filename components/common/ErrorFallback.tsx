d2a4e0f1c05a14fbe0bfc0ab8a31e3b1c326c6a1
Gyen Abubakar <indehyde@gmail.com>
2022-02-26 22:21:41 +0000

    ErrorFallback: accept children prop

--- components/common/ErrorFallback.tsx
+++ components/common/ErrorFallback.tsx
@@ -5,37 +5,43 @@ import { useRouter } from 'next/router';
 import illustrationNotFound from '~/assets/illustrations/not-found.svg';
 
 interface Props {
-  title: string;
-  message: string;
+  title?: string;
+  message?: string;
 }
 
-const ErrorFallback: React.FC<Props> = ({ message, title }) => {
+const ErrorFallback: React.FC<Props> = ({ message, title, children }) => {
   const router = useRouter();
 
   return (
     <div className="error mt-24">
-      <div className="w-10/12 h-32 relative mx-auto">
-        <Image src={illustrationNotFound} layout="fill" />
-      </div>
+      {children}
 
-      <div className="description mt-12">
-        <h1 className="text-xl md:text-2xl font-medium text-darkgray text-center">
-          {title}
-        </h1>
+      {!children && (
+        <>
+          <div className="w-10/12 h-32 relative mx-auto">
+            <Image src={illustrationNotFound} layout="fill" />
+          </div>
 
-        <p className="text-gray-500 text-center mt-5 text-base md:text-lg">
-          {message}
-        </p>
+          <div className="description mt-12">
+            <h1 className="text-xl md:text-2xl font-medium text-darkgray text-center">
+              {title}
+            </h1>
 
-        <div className="text-center mt-12">
-          <button
-            className="px-10 py-3 rounded-lg bg-main text-white font-medium text-sm"
-            onClick={() => router.push('/app/')}
-          >
-            Go home
-          </button>
-        </div>
-      </div>
+            <p className="text-gray-500 text-center mt-5 text-base md:text-lg">
+              {message}
+            </p>
+
+            <div className="text-center mt-12">
+              <button
+                className="px-10 py-3 rounded-lg bg-main text-white font-medium text-sm"
+                onClick={() => router.push('/app/')}
+              >
+                Go home
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
