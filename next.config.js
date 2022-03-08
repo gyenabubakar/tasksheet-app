65e42ac4e513544ef1045ef819043db3b24c1534
Gyen Abubakar <indehyde@gmail.com>
2022-03-08 11:03:44 +0000

    fix next.js / webpack config

--- next.config.js
+++ next.config.js
@@ -12,7 +12,9 @@ const nextConfig = {
     // Important: return the modified config
     return {
       ...config,
-      allowedHosts: 'all',
+      devServer: {
+        allowedHosts: 'all',
+      },
     };
   },
 };
